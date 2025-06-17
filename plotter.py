7cd141cd24738c269f4255fe5a906681d84ab127
W-physics <w.montano@uniandes.edu.co>
2025-06-17 16:05:00 -0500

    making_plotter

--- plotter.py
+++ plotter.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-loss_hist_train = pd.read_csv("data\d-loss_hist_train.csv")
-loss_hist_valid = pd.read_csv("data\d-loss_hist_valid.csv")
+plt.style.use('bmh')
 
-plt.plot(loss_hist_train,label='train')
-plt.plot(loss_hist_valid,label='valid')
-plt.legend()
-plt.savefig("/home/william/Github/Tesis/figures/train_valid.svg")
+loss_hist_train = pd.read_csv("data\d-loss_hist_train.csv", usecols=[1])
+loss_hist_valid = pd.read_csv("data\d-loss_hist_valid.csv", usecols=[1])
+
+fig, ax = plt.subplots()
+
+ax.set_xlabel("epochs")
+ax.set_ylabel("MSE")
+ax.set_title("Training and validation errors")
+
+ax.plot(loss_hist_train,label='train')
+ax.plot(loss_hist_valid,label='valid')
+ax.legend(fontsize='large')
+
+plt.savefig("figures\losses.pdf")
