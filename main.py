5ae7c61916dc9c412518bd5e6b81794aeed38b15
W-physics <w.montano@uniandes.edu.co>
2025-06-20 18:41:13 -0500

    adding venv to gitignore

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from train_and_generate.train_generate import TrainGenerate
 from save_plot.plotter import PlotTrainval, PlotViolin
 
 def main():
+        
+    TrainGenerate(drift_term=0, noise_level=2)
+    PlotTrainval(1)
+    PlotViolin(1)
 
-    for i in range(10):
-        print(f"Iteration {i+1} of 10")
-        TrainGenerate(drift_term=0.01, noise_level=0.1**i)
-        PlotTrainval(i)
-        PlotViolin(i)
+    TrainGenerate(drift_term=0, noise_level=1)
+    PlotTrainval(2)
+    PlotViolin(2)
+
+    TrainGenerate(drift_term=0, noise_level=0.1)
+    PlotTrainval(3)
+    PlotViolin(3)
+
+
 
 main()
