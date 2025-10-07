9dd59b1d53f7d032dd2ec1248ddff0f8d9b3d783
William <w.montano@uniandes.edu.co>
2025-10-07 14:04:35 -0500

    merging Tellez changes

--- forward_process/generate_noised_data.py
+++ forward_process/generate_noised_data.py
@@ -14,24 +14,24 @@ def ForwardProcessOriginal_no_optimizada(timesteps, initial_data):
     for each data point in the initial_data array.
     """
     ndata = len(initial_data)
-    features = np.zeros((ndata, timesteps, 2))
-    noises = np.random.normal(0,1,size=(ndata,timesteps))
+    features = np.zeros((ndata, timesteps,  2))
+    noises = np.random.normal(0,1,size=((ndata,timesteps), timesteps))
     beta = BetaSchedule(timesteps)
     alpha = 1 - beta
+    alpha_bar = np.cumprod(alpha)
     alpha_bar = np.cumprod(alpha)
     times = np.arange(timesteps)
 
     # TO DO: optimizar: esto se puede hacer más rápidosin dos loops
     for i in range(ndata):
 
-        # usar np.prod(alpha[:time]) genera un bug pues el primer elemento es 1
-        # en vez alpha[0] 
-        # es mejor usar cumprod
 
         # Codigo no optimizado
         for j in range(timesteps):
             noise = noises[i,j]
+            noise = noises[i,j]
             time = times[j]
+            noised_data = initial_data[i] * np.sqrt(alpha_bar[time]) + np.sqrt(1 - alpha_bar[time])*noise
             noised_data = initial_data[i]*np.sqrt(alpha_bar[time]) + np.sqrt((1 - alpha_bar[time]))*noise
             # Error inicial: la desviación estándar del ruido es sqrt(1 - alpha_bar y no 1-alpha_bar
             # noised_data = initial_data[i]*np.sqrt(alpha_bar[time]) + (1 - alpha_bar[time])*noise
@@ -61,6 +61,7 @@ def ForwardProcess(timesteps, initial_data):
     features[:,:,0] = noised_data
     features[:,:,1] = times
 
+    return  features, noises
     return  features, noises
 
 def GenerateNoisedData(timesteps, ndata, initial_distribution):
