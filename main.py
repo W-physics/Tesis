b8b6e60d34dbc968bdcca48949d2b49bdb51869d
W-physics <w.montano@uniandes.edu.co>
2026-04-26 17:12:56 -0500

    correcting_main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from farming_data.train_generate import TrainAndGenerateDatasets
 from farming_data.find_maximums import FindMaximumOfGenerations
 from farming_data.projection import Project
+from save_plot.save_files import SaveCSV
 
 import numpy as np
 
@@ -16,12 +17,13 @@ def main():
 
         for c in list_c:
         
-            distros = TrainAndGenerateDatasets(ndata, dimension=d)
+            distros = TrainAndGenerateDatasets(ndata, dimension=d, c=c)
             projections = Project(distros=distros,
                                   ndata=ndata,
-                                  timesteps=1000
+                                  timesteps=1000,
                                   dimension=d)
-
+            
+            SaveCSV(projections, f'projetions_d={d}_c={c}')
             FindMaximumOfGenerations(projections, dimension=d, c=c, repetitions=1)
     
 main()
